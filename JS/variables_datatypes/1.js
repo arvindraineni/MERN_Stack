218be4878a6734d87a87503584565daedd5528a2
Arvind Raineni <arvindraineni@gmail.com>
2021-11-14 11:04:28 +0530

    feat: added variables and datatypes

---
+++ JS/variables_datatypes/1.js
@@ -0,0 +1,30 @@
+// declare variables
+
+let a;
+
+//assign value
+
+a=44;
+
+//print something to console
+
+console.log('print console')
+console.log('Hello')
+console.log(a)
+console.log('value of a is ', a)
+
+
+//datatypes
+
+a=4;
+console.log(typeof(a))
+
+a="hello";
+console.log(typeof(a))
+
+
+a=true;
+console.log(typeof(a))
+
+
+let name;
